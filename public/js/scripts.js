539adaec75eef30c87b24d9a680c75c28e3203ff
ninapetrop <thebearcough@gmail.com>
2016-05-23 21:05:31 -0400

    style updates, top-bar ticker

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-
-  $('#official-staff > #skunix').click(function() {
-    $(this).addClass("active").siblings(".mh-member").removeClass("active");
-    $('#mh-member-profile > #skunix').addClass("active").siblings(".mh-member-profile").removeClass("active");
-  });
-
-  // when user clicks a profile (#official-staff > #profile)
-  // get specific name by id and store in variable
-
-  // remove "active" class from all sibling elements
-  // add "active" class to corresponding #mh-member-profile > #ID-matches-variable
+// Current event ticker
+var width = $('.ticker-text').width(),
+    containerwidth = $('.ticker-container').width(),
+    left = containerwidth;
+$(document).ready(function(e){
+	function tick() {
+        if(--left < -width){
+            left = containerwidth;
+        }
+        $(".ticker-text").css("margin-left", left + "px");
+        setTimeout(tick, 20);
+      }
+      tick();
 });
