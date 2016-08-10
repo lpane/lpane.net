957d6fad1eb5a09b80220e0372be62816a28c342
chandwer <chris.handwerker@gmail.com>
2016-08-10 12:49:09 -0400

    use helper plugins in MostlyHarmless.pm

--- lib/MostlyHarmless.pm
+++ lib/MostlyHarmless.pm
@@ -1,50 +1,19 @@
 package MostlyHarmless;
 
 use Mojo::Base 'Mojolicious';
-use Mojo::Redis2;
-use Mojo::JSON qw( decode_json encode_json );
-
-use LWP::UserAgent;
 
 use MostlyHarmless::Model::Event;
-use MostlyHarmless::Model::Server::Ark;
 
 sub startup {
 	my ($self) = @_;
 
-	$self->defaults({
-		sidebar	=> [
-			{ name => "Home", href	=> "/" },
-			{ name => "Register", href	=> "/register" },
-			{ name => "Games", href	=> "/games" },
-			{ name => "About", href => "/about" }
-		],
-		error   => {},
-		fields  => {},
-		header  => 1,
-		autosubmit => 0
-	});
-
-	# Array of game server classes
-	$self->helper( servers => sub { @{ state $servers = sub {
-		my ($self) = @_;
-		my @servers;
-
-		push( @servers, MostlyHarmless::Model::Server::Ark->new({
-			title => "Ark: Survival - The Center",
-			host  => "ark-center"
-		}));
-
-		push( @servers, MostlyHarmless::Model::Server::Ark->new({
-			title => "Ark: Survival - The Island",
-			host  => "ark-island"
-		}));
-
-		return \@servers;
-	}->()}});
+	$self->plugin('MostlyHarmless::Plugin::Defaults');	# Default application settings
+	$self->plugin('MostlyHarmless::Plugin::Servers');	# Game server classes
 
 	$self->hook( before_dispatch => sub {
 		my ($c) = @_;
+		# Read event config for every request
+		# TODO only instantiate this for requests under the main controller
 		$c->stash( event => MostlyHarmless::Model::Event->new() );
 	});
 
@@ -53,5 +22,9 @@ sub startup {
 	$r->route('/')->to('main#index');
 	$r->route('/games')->to('main#games');
 	$r->route('/about')->to('main#about');
+
+	my $login = $r->any('/login')->to( controller => 'login' );
+	$login->get('/')->to( action => 'claim_identity' );
+	$login->get('/check')->to( action => 'check_identity' );
 }
 1;
